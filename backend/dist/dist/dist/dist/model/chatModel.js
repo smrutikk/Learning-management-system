c06df7d41319edb657867d987e664e915fa22bd2
Smrutik <117387914+smrutikk@users.noreply.github.com>
2024-11-08 10:52:07 +0530

    updated depencies and related packages

---
+++ backend/dist/dist/dist/dist/model/chatModel.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const chatMessageSchema = new mongoose.Schema({
+  sender: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
+module.exports = ChatMessage;
---
+++ backend/dist/dist/dist/dist/model/multiplefile.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mulitipleFileSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  files: [Object]
+}, {
+  timestamps: true
+});
+module.exports = mongoose.model('MultipleFile', mulitipleFileSchema);
